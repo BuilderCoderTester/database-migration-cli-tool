897ca4797033690fdc68888d572b98526597b19c
anurag <sarkaranurag556@gmail.com>
2026-06-07 12:12:59 +0530

    files changes

--- demo/src/main/java/com/project/demo/component/UpMigrationService.java
+++ demo/src/main/java/com/project/demo/component/UpMigrationService.java
@@ -1,10 +1,12 @@
 package com.project.demo.component;
 
+import com.project.demo.enumuration.Status;
 import com.project.demo.model.MigrationScript;
 import com.project.demo.utility.Helper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.sql.Connection;
 import java.sql.SQLException;
 
 @Service
@@ -15,4 +17,15 @@ public class UpMigrationService {
     public void upMigrationScript(MigrationScript script, long connectionId, long startTime) throws SQLException {
         helper.applyVersioned(script,connectionId,startTime);
     }
+
+    public void saveMigrationRecord(MigrationScript script, long connectionId, long startTime, boolean value , Connection connection) throws SQLException {
+            helper.saveMigrationRecord(script, connectionId, System.currentTimeMillis() - startTime, false,connection);
+
+    }
+
+    public void updateMigrationScript(MigrationScript scriptVersion,long startTime ,long connectionId, Status status, long executionTime) throws SQLException {
+                    helper.updateMigrationStatus(scriptVersion.getVersion(), connectionId, Status.PASSED,
+                    System.currentTimeMillis() - startTime);
+    }
+
 }
